13f86eda87bc528db51268ed38323449c9afec66
noellamwanzia <39863136+noellamwanzia@users.noreply.github.com>
2022-03-02 12:34:44 +0300

    :+1: create paul.sol.js

---
+++ paul.sol.js
@@ -0,0 +1,7 @@
+// How to capitalize characters function()
+ 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  console.log(capitalizeFirstLetter('paul')); 
